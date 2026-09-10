5e92f3f1d040f159cab511925bba3d25140346d4
Thalles C. Nascimento <ana.pauulajc76@gmail.com>
2026-09-10 16:13:51 -0300

    feat: dockerfile e docker-compose criados
    infra: banco de dados migrado para postgres
    
    feat: views.py configurado para listar os posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def list_posts(request):
+    post_publicados = Post.objects.filter(status="publicado").order_by("-criado_em")
+
+    return render(
+        request, 'posts/list_posts.html', {
+            "posts": post_publicados
+        }
+    )
