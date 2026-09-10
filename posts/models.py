e43565b4ae8cf4cb765ae333ca46bba09308cbba
Thalles C. Nascimento <ana.pauulajc76@gmail.com>
2026-09-10 14:34:41 -0300

    feat: blog iniciado
    feat: models feito

---
+++ posts/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+class Tag(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('rascunho', 'Rascunho'),
+        ('revisao', 'Em revisão'),
+        ('publicado', 'Publicado')
+    )
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='rascunho')
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Comentario(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comentarios")
+    autor = models.CharField(max_length=100)
+    texto = models.TextField()
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentário de {self.autor} em {self.post.titulo}"
