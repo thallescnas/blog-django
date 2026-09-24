8ca88c02a2492e49eafdc309ab563201b46d0d8a
thallescnas <ana.pauulajc76@gmail.com>
2026-09-23 21:19:48 -0300

    feat: adição de categoria como uma opção de escolher o que quer fazer
    feat: adição de erro no forms.py

--- posts/forms.py
+++ posts/forms.py
@@ -5,3 +5,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['titulo', 'conteudo', 'categoria', 'tags', 'autor']
+    def clean_conteudo(self):
+        conteudo = self.cleaned_data.get("conteudo")
+        
+        if conteudo and len(conteudo.strip()) < 50:
+            raise forms.ValidationError(f'O conteudo deve ter no minomo 50 caracteres.'
+                                        f'Você tem apenas {len(conteudo.strip())}')            
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,22 @@
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from .models import Post
+from .models import Post, Categoria
 from .forms import PostForm
 
 def list_posts(request : HttpRequest):
     posts = Post.objects.filter(status="publicado").order_by('-criado_em')
+    categorias = Categoria.objects.all()
     q = request.GET.get('q', '')
+    categoria = request.GET.get('categorias', '')
     
     if q:
         posts = posts.filter(Q(titulo__icontains=q))
+    if categoria:
+        posts = posts.filter(categoria__nome=categoria)
     return render(request, 'posts/list_posts.html', {
-        "posts": posts
+        "posts": posts,
+        "categorias": categorias
     })
 def create_post(request : HttpRequest):
     if request.method == "POST":
